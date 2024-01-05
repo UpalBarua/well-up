22000636113eff7b07aed8f7c9b780de0bc0eec6
UpalBarua <upalbarua52@gmail.com>
2024-01-05 13:17:19 +0600

    completed the hero section

---
+++ client/src/components/hero.tsx
@@ -0,0 +1,122 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { ChevronDown, Code, Search } from 'lucide-react';
+import Image from 'next/image';
+import { Button } from './ui/button';
+import { Input } from './ui/input';
+
+const Hero = () => {
+  return (
+    <section className="pb-10 pt-5 grid grid-cols-2 gap-x-12 items-center mx-auto max-w-6xl lg:pb-28 lg:pt-8">
+      <div className="space-y-2.5 lg:space-y-4">
+        <h1 className="text-3xl font-bold tracking-tight lg:text-5xl lg:leading-[1.125] dark:text-white">
+          Empowering Futures, Elevating Skills, Welcome to{' '}
+          <span className="text-secondary-500">Well-Up!</span>
+        </h1>
+        <p className="leading-relaxed text-neutral-500 max-w-[90%] dark:text-neutral-200">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque ullam
+          incidunt dolores nostrum voluptatibus eligendi velit iusto laboriosam,
+          official.
+        </p>
+        <div className="bg-white rounded-xl p-3 border border-neutral-50/50 shadow-sm space-y-2.5 dark:bg-neutral-950 dark:border-neutral-800/50">
+          <form className="flex items-center gap-x-2">
+            <Input type="text" className="" placeholder="Search Courses" />
+            <Button size="icon" className="h-12 w-14">
+              <Search />
+            </Button>
+          </form>
+          <div className="flex items-center gap-x-2.5">
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button
+                  variant="outline"
+                  className="gap-x-3 h-auto py-3 w-full">
+                  <Code className="h-5 w-5" />
+                  <span className="capitalize font-medium">Category</span>
+                  <ChevronDown className="ms-2.5 h-5 w-5" />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="w-[16.25rem]" side="bottom">
+                <DropdownMenuItem>Option 1</DropdownMenuItem>
+                <DropdownMenuItem>Option 1</DropdownMenuItem>
+                <DropdownMenuItem>Option 1</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button
+                  variant="outline"
+                  className="gap-x-3 h-auto py-3 w-full">
+                  <Code className="h-5 w-5" />
+                  <span className="capitalize font-medium">Category</span>
+                  <ChevronDown className="ms-2.5 h-5 w-5" />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="w-[16.25rem]" side="bottom">
+                <DropdownMenuItem>Option 1</DropdownMenuItem>
+                <DropdownMenuItem>Option 1</DropdownMenuItem>
+                <DropdownMenuItem>Option 1</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button
+                  variant="outline"
+                  className="gap-x-3 h-auto py-3 w-full">
+                  <Code className="h-5 w-5" />
+                  <span className="capitalize font-medium">Category</span>
+                  <ChevronDown className="ms-2.5 h-5 w-5" />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="w-[16.25rem]" side="bottom">
+                <DropdownMenuItem>Option 1</DropdownMenuItem>
+                <DropdownMenuItem>Option 1</DropdownMenuItem>
+                <DropdownMenuItem>Option 1</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+        </div>
+      </div>
+      <div className="grid grid-cols-12 grid-rows-12 gap-1 h-[30rem]">
+        <div className="relative col-span-6 row-start-2 row-span-6">
+          <Image
+            src="https://source.unsplash.com/random/?code"
+            alt="hero image"
+            className="rounded-xl object-center object-cover"
+            fill
+          />
+        </div>
+        <div className="relative col-start-7 col-span-5 row-span-6 ">
+          <Image
+            src="https://source.unsplash.com/random/?construction"
+            alt="hero image"
+            className="rounded-xl object-center object-cover"
+            fill
+          />
+        </div>
+        <div className="relative col-start-2 col-span-5 row-start-8 row-span-full">
+          <Image
+            src="https://source.unsplash.com/random/?blueprint"
+            alt="hero image"
+            className="rounded-xl object-center object-cover"
+            fill
+          />
+        </div>
+        <div className="relative col-start-7 col-span-full row-span-5">
+          <Image
+            src="https://source.unsplash.com/random/?electrician"
+            alt="hero image"
+            className="rounded-xl object-center object-cover"
+            fill
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,11 +1,13 @@
-import Catagories from "@/components/Catagories/Catagories";
-import FeaturedCourses from "@/components/FeaturedCourses/FeaturedCourses";
-import Members from "@/components/Members/Members";
-import { Button } from "@/components/ui/button";
+import Catagories from '@/components/Catagories/Catagories';
+import FeaturedCourses from '@/components/FeaturedCourses/FeaturedCourses';
+import Members from '@/components/Members/Members';
+import Hero from '@/components/hero';
+import { Button } from '@/components/ui/button';
 
 const HomePage = () => {
   return (
-    <main>
+    <main className="container">
+      <Hero />
       <Members></Members>
       <Catagories></Catagories>
       <FeaturedCourses></FeaturedCourses>
