8e8b6f63e6b3fc7dbaba2ec070cd7d94cdbcd60a
niaz abir <niazabir55@gmail.com>
2024-01-07 18:09:15 +0600

    about hero commit

---
+++ client/src/pages/about-us.tsx
@@ -0,0 +1,12 @@
+import AboutBanner from "@/components/AboutBanner";
+import React from "react";
+
+const AboutUs = () => {
+  return (
+    <div>
+      <AboutBanner></AboutBanner>
+    </div>
+  );
+};
+
+export default AboutUs;
