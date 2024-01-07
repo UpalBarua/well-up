fe4d1de33064a949ddff8c68c2cb5f6715210e2c
niaz abir <niazabir55@gmail.com>
2024-01-07 12:49:17 +0600

    course commit

--- client/src/pages/success-story.tsx
+++ client/src/pages/success-story.tsx
@@ -158,7 +158,7 @@ const Success = () => {
               >
                 <div className="px-4 py-12 rounded-t-lg sm:px-8 md:px-12 ">
                   <p className="relative px-6 py-1 text-lg italic text-center dark:text-gray-100">
-                    <svg
+                    {/* <svg
                       xmlns="http://www.w3.org/2000/svg"
                       viewBox="0 0 512 512"
                       fill="currentColor"
@@ -166,9 +166,9 @@ const Success = () => {
                     >
                       <path d="M232,246.857V16H16V416H54.4ZM48,48H200V233.143L48,377.905Z"></path>
                       <path d="M280,416h38.4L496,246.857V16H280ZM312,48H464V233.143L312,377.905Z"></path>
-                    </svg>
+                    </svg> */}
                     {reviews?.review}
-                    <svg
+                    {/* <svg
                       xmlns="http://www.w3.org/2000/svg"
                       viewBox="0 0 512 512"
                       fill="currentColor"
@@ -176,7 +176,7 @@ const Success = () => {
                     >
                       <path d="M280,185.143V416H496V16H457.6ZM464,384H312V198.857L464,54.1Z"></path>
                       <path d="M232,16H193.6L16,185.143V416H232ZM200,384H48V198.857L200,54.1Z"></path>
-                    </svg>
+                    </svg> */}
                   </p>
                 </div>
                 <div className="flex flex-col items-center justify-center p-8 rounded-b-lg dark:bg-violet-400 dark:text-gray-900">
